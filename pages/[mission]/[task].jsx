ded5d2282eb770209ecb8e0f60d72455990b6170
freka333 <freka333@gmail.com>
2023-03-24 21:31:36 +0100

    await test

--- pages/[mission]/[task].jsx
+++ pages/[mission]/[task].jsx
@@ -32,24 +32,34 @@ export default function ProjectPage({ foundMission, foundTask, hasError, userInf
 }
 
 export const getServerSideProps = async (context) => {
+
+    console.log("before getServerSession await")
     const session = await getServerSession(context.req, context.res, authOptions);
+    console.log("after getServerSession await")
+
     if (!session) {
         return { redirect: { destination: '/', permanent: false } }
     }
     await dbConnect();
+
+    console.log("before initInfo(session.user) await")
     const info = await initInfo(session.user);
+    console.log("after initInfo(session.user) await")
+
     const { userInfo, characters } = info;
 
+    console.log("before Mission.find await")
     const missionsResult = await Mission.find({})
+    console.log("after Mission.find await")
 
-    const missions = missionsResult.map((doc) => {
+    /*const missions = missionsResult.map((doc) => {
         const mission = doc.toObject();
         mission._id = mission._id.toString();
         mission.tasks = JSON.parse(JSON.stringify(mission.tasks));
         return mission
     })
 
-    /*const foundMission = missions.find(item => item.title === context.params?.mission)
+    const foundMission = missions.find(item => item.title === context.params?.mission)
     let foundTask;
     if (foundMission) {
         foundTask = foundMission.tasks.find(item => item.path === context.params?.task);
