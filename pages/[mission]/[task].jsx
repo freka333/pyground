18abfc363ce7590ad708f3b35529da6ead80803a
freka333 <freka333@gmail.com>
2023-03-24 21:20:29 +0100

    test

--- pages/[mission]/[task].jsx
+++ pages/[mission]/[task].jsx
@@ -40,28 +40,28 @@ export const getServerSideProps = async (context) => {
     const info = await initInfo(session.user);
     const { userInfo, characters } = info;
 
-    const missionsResult = await Mission.find({})
-
-    const missions = missionsResult.map((doc) => {
-        const mission = doc.toObject();
-        mission._id = mission._id.toString();
-        mission.tasks = JSON.parse(JSON.stringify(mission.tasks));
-        return mission
-    })
-
-    const foundMission = missions.find(item => item.title === context.params?.mission)
-    let foundTask;
-    if (foundMission) {
-        foundTask = foundMission.tasks.find(item => item.path === context.params?.task);
-    }
-
-    if (!foundTask) {
-        return {
-            props: { hasError: true }
-        }
-    }
-
+    /* const missionsResult = await Mission.find({})
+ 
+     const missions = missionsResult.map((doc) => {
+         const mission = doc.toObject();
+         mission._id = mission._id.toString();
+         mission.tasks = JSON.parse(JSON.stringify(mission.tasks));
+         return mission
+     })
+ 
+     const foundMission = missions.find(item => item.title === context.params?.mission)
+     let foundTask;
+     if (foundMission) {
+         foundTask = foundMission.tasks.find(item => item.path === context.params?.task);
+     }
+ 
+     if (!foundTask) {
+         return {
+             props: { hasError: true }
+         }
+     }
+ */
     return {
-        props: { foundMission, foundTask, userInfo: JSON.parse(JSON.stringify(userInfo)), characters }
+        props: { userInfo: JSON.parse(JSON.stringify(userInfo)), characters }
     }
 }
