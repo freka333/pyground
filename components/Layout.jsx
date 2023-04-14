fbcb73f752121210f161860ac6431ce52fc02164
freka333 <freka333@gmail.com>
2023-04-14 11:17:46 +0200

    add title in Head component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,12 +1,19 @@
+import Head from "next/head";
 import Header from "./Header";
 import WelcomeDialog from "./WelcomeDialog";
 
 export default function Layout({ children, user, characters }) {
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }} >
-            <Header user={user} />
-            <WelcomeDialog user={user} characters={characters} />
-            {children}
-        </div>
+        <>
+            <Head>
+                <title>PyGround</title>
+                <meta property="og:title" content="My page title" key="title" />
+            </Head>
+            <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }} >
+                <Header user={user} />
+                <WelcomeDialog user={user} characters={characters} />
+                {children}
+            </div>
+        </>
     )
 }
