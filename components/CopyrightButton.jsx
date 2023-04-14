e2516e8e7a459e9a50167124261266919e668e10
freka333 <freka333@gmail.com>
2023-04-14 11:38:07 +0200

    add ExternalContentDialog to Login page

--- components/CopyrightButton.jsx
+++ components/CopyrightButton.jsx
@@ -2,12 +2,13 @@ import { Button, Typography } from "@mui/material";
 import ExternalContentDialog from "./ExternalContentDialog";
 import CopyrightIcon from "@mui/icons-material/Copyright";
 
-export default function CopyrightButton({ open, handleOpen, handleClose }) {
+export default function CopyrightButton({ open, handleOpen, handleClose, color }) {
+    const txtColor = color || 'primary.dark';
     return (
         <div style={{ display: 'flex', alignSelf: 'end', marginRight: '10px' }}>
             <Button disableElevation disableRipple onClick={handleOpen} sx={{ ':hover': { bgcolor: 'transparent' } }}>
-                <CopyrightIcon sx={{ color: 'primary.dark', fontSize: 16 }} />
-                <Typography color='primary.dark'> Képek forrása </Typography>
+                <CopyrightIcon sx={{ color: txtColor, fontSize: 16 }} />
+                <Typography color={txtColor}> Képek forrása </Typography>
             </Button>
             <ExternalContentDialog open={open} handleClose={handleClose} />
         </div>
--- components/Login.jsx
+++ components/Login.jsx
@@ -1,6 +1,7 @@
 import { Box, Button } from "@mui/material";
 import { signIn } from "next-auth/react";
 import { useState } from "react";
+import CopyrightButton from "./CopyrightButton";
 
 const styles = {
     container: {
@@ -15,10 +16,19 @@ const styles = {
 
 export default function Login() {
     const [disable, setDisable] = useState(false);
+    const [open, setOpen] = useState(false);
 
     const handleSignIn = () => {
         setDisable(true);
         signIn('google');
+    }
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
     }
 
     return (
@@ -30,6 +40,9 @@ export default function Login() {
             }}>
                 <img src='images/logo.png' alt='Pyground logo' width='200' style={{ padding: '10px' }} />
                 <Button variant='contained' disabled={disable} color='secondary' onClick={handleSignIn}>Bejelentkezés Google fiókkal</Button>
+            </Box>
+            <Box sx={{ position: 'fixed', bottom: 0, right: 0 }}>
+                <CopyrightButton open={open} handleOpen={handleOpen} handleClose={handleClose} color='secondary.main' />
             </Box>
         </div>
     )
