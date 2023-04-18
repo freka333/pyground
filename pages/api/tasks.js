7d7454dd646a8391d228d0acd2cd23b0cb06eb47
freka333 <freka333@gmail.com>
2023-04-18 16:32:21 +0200

    remove GET method from /api/tasks

--- pages/api/tasks.js
+++ pages/api/tasks.js
@@ -4,25 +4,6 @@ import Mission from "@/models/Mission";
 export default async function handler(req, res) {
 
     switch (req.method) {
-        case 'GET': {
-            await dbConnect();
-            const missionResult = await Mission.find({});
-
-            const missions = missionResult.sort((a, b) => a.num - b.num).map(doc => {
-                const mission = doc.toObject();
-                if (!mission.disabled) {
-                    mission._id = mission._id.toString();
-                    return {
-                        id: mission._id,
-                        number: mission.num,
-                        title: mission.title,
-                        tasks: mission.tasks
-                    }
-                }
-            })
-            res.status(200).json(missions);
-            break
-        }
         case 'POST': {
             await dbConnect();
 
